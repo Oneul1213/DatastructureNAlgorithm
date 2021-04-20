e4210f3c3a12a4cf9687409b33ed4e0e15f2482e
Oneul <toda1213@naver.com>
2021-04-21 00:06:52 +0900

    이중 해쉬 C 구현
    Person 구조체를 데이터로 갖는 이중 해쉬 테이블을 C로 구현.

---
+++ codes/DoubleHashTable/C/DobleHashMain.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Person.h"
+#include "Table.h"
+
+int MyHashFunc1(int k)
+{
+	return k % 100;
+}
+
+int MyHashFunc2(int k)
+{
+	return 1 + (k % 7);
+}
+
+int main(void)
+{
+	Table myTbl;
+	Person* np;
+	Person* sp;
+	Person* rp;
+
+	TBLInit(&myTbl, MyHashFunc1, MyHashFunc2);
+
+	np = MakePersonData(20120003, "LEE", "Seoul");
+	TBLInsert(&myTbl, GetSSN(np), np);
+
+	np = MakePersonData(20130012, "KIM", "Jeju");
+	TBLInsert(&myTbl, GetSSN(np), np);
+
+	np = MakePersonData(20170049, "HAN", "Kangwon");
+	TBLInsert(&myTbl, GetSSN(np), np);
+
+	sp = TBLSearch(&myTbl, 20120003);
+	if (sp != NULL)
+		ShowPerInfo(sp);
+
+	sp = TBLSearch(&myTbl, 20130012);
+	if (sp != NULL)
+		ShowPerInfo(sp);
+
+	sp = TBLSearch(&myTbl, 20170049);
+	if (sp != NULL)
+		ShowPerInfo(sp);
+
+	rp = TBLDelete(&myTbl, 20120003);
+	if (rp != NULL)
+		free(rp);
+
+	rp = TBLDelete(&myTbl, 20130012);
+	if (rp != NULL)
+		free(rp);
+
+	rp = TBLDelete(&myTbl, 20170049);
+	if (rp != NULL)
+		free(rp);
+
+	return 0;
+}
---
+++ codes/DoubleHashTable/C/Table.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Table.h"
+
+void TBLInit(Table* pt, HashFunc* f1, HashFunc* f2)
+{
+	int i;
+
+	for (i = 0; i < MAX_TBL; i++)
+		(pt->tbl[i]).status = EMPTY;
+
+	pt->hf1 = f1;
+	pt->hf2 = f2;
+}
+
+void TBLInsert(Table* pt, Key k, Value v)
+{
+	int hv = pt->hf1(k);
+
+	// 충돌 발생 시 빈 자리를 찾을 때 까지 반복
+	while (pt->tbl[hv].status == INUSE || pt->tbl[hv].status == DELETED)
+	{
+		hv = hv + pt->hf2(k);
+	}
+
+	pt->tbl[hv].val = v;
+	pt->tbl[hv].key = k;
+	pt->tbl[hv].status = INUSE;
+}
+
+Value TBLDelete(Table* pt, Key k)
+{
+	int hv = pt->hf1(k);
+
+	while (pt->tbl[hv].status == DELETED)
+		hv = hv + pt->hf2(k);
+
+	if ((pt->tbl[hv]).status != INUSE)
+		return NULL;
+	else
+	{
+		(pt->tbl[hv]).status = DELETED;
+		return (pt->tbl[hv]).val;
+	}
+}
+
+Value TBLSearch(Table* pt, Key k)
+{
+	int hv = pt->hf1(k);
+
+	while (pt->tbl[hv].status == DELETED)
+		hv = hv + pt->hf2(k);
+
+	if ((pt->tbl[hv]).status != INUSE)
+		return NULL;
+	else
+		return (pt->tbl[hv]).val;
+}
